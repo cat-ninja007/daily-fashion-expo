3974496b74abe91218a922684f3542691ddfef68
cat-ninja007 <150651906+cat-ninja007@users.noreply.github.com>
2025-02-03 15:06:59 +0800

    Finish Linking and Zoom in-out

--- app/ShowProductScreen.tsx
+++ app/ShowProductScreen.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect, useState} from 'react'
-import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { FC, useEffect, useState } from 'react';
+import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { MediaComponent } from '@/components/MediaComponent';
 import { Linking } from 'react-native';
 
@@ -17,146 +17,114 @@ interface Product {
   phoneNumber: string;
 }
 
-
-const ShowProductScreen:FC = () => {
+const ShowProductScreen: FC = () => {
   const params = useLocalSearchParams();
-  const [ products, setProducts ] = useState<Product[]>([]);
-  const [ isBuy, setIsBuy ] = useState(false);
-  const [ contact, setContact ] = useState({
+  const router = useRouter();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isBuy, setIsBuy] = useState(false);
+  const [contact, setContact] = useState({
     phoneNumber: '',
     instagram: '',
-    facebook: ''
-  })
+    facebook: '',
+  });
 
-  const buyProduct = (
-    whatsapp: string, 
-    instagramId: string, 
-    facebookId: string
-  ) => {
+  const buyProduct = (whatsapp: string, instagramId: string, facebookId: string) => {
     setContact({
       phoneNumber: whatsapp,
       instagram: instagramId,
-      facebook: facebookId
+      facebook: facebookId,
     });
     setIsBuy(true);
-  }
+  };
 
-  // const handleImageZoomPress = (imagePath: string) => {
-  //   const image
-  // }
-
-
-  const onClickMedia = (type:string ) => {
-    if(type === 'whatsapp'){
+  const onClickMedia = (type: string) => {
+    if (type === 'whatsapp') {
       Linking.openURL(`https://wa.me/${contact.phoneNumber}`);
-    } else if(type === 'instagram'){
+    } else if (type === 'instagram') {
       Linking.openURL(`https://instagram.com/${contact.instagram}`);
-    } else if(type === 'facebook'){
+    } else if (type === 'facebook') {
       Linking.openURL(`https://facebook.com/${contact.facebook}`);
     }
-  }
+  };
 
-  
+  const handleImagePress = (imagePath: string) => {
+    router.push({
+      pathname: '/ImageZoomScreen', // Adjust this based on your file structure
+      params: { imagePath },
+    });
+  };
+
   useEffect(() => {
     const data = params.products as unknown as string;
     try {
       const parsedProducts = JSON.parse(data) as Product[];
       setProducts(parsedProducts);
-    } catch(error){
-      console.error("Failed to parse products: ", error);
+    } catch (error) {
+      console.error('Failed to parse products: ', error);
     }
-    // console.log('This is the parameter', data)  
-  }, [params.products])
-  useEffect(() => {
-    console.log(isBuy)
-  }, [])
+  }, [params.products]);
+
   return (
     <View style={styles.mainContainer}>
-      <FlatList 
+      <FlatList
         data={products}
         contentContainerStyle={styles.flatListContainer}
         keyExtractor={(item) => item.id.toString()}
         ListEmptyComponent={() => <Text style={styles.emptyListText}>No products found.</Text>}
         renderItem={({ item }) => (
-          <TouchableOpacity 
-              style={styles.itemButton}>
-              onPress
+          <TouchableOpacity style={styles.itemButton}>
             <View style={styles.productContainer}>
-              <Image 
-                style={styles.image}
-                source={{uri: item.imagePath}}
-                />
+              <TouchableOpacity onPress={() => handleImagePress(item.imagePath)}>
+                <Image style={styles.image} source={{ uri: item.imagePath }} />
+              </TouchableOpacity>
               <View style={styles.textContainer}>
                 <Text style={styles.title}>{item.productName}</Text>
                 <Text style={styles.text}>{item.description}</Text>
                 <Text style={styles.text}>{item.price}</Text>
               </View>
             </View>
-            <TouchableOpacity
-              onPress={() => buyProduct(item.phoneNumber, item.instagram, item.facebook)}
-            >
+            <TouchableOpacity onPress={() => buyProduct(item.phoneNumber, item.instagram, item.facebook)}>
               <AntDesign name="shoppingcart" size={30} color="black" />
             </TouchableOpacity>
           </TouchableOpacity>
         )}
       />
 
-      {
-        isBuy ?
-          <View style={styles.modalContainer}>
-            <View style={styles.box}>
-              <TouchableOpacity 
-                onPress={() => setIsBuy(false)}
-                style={styles.cancel}
-              >
-                <AntDesign name="close" size={30} color="black" />
-              </TouchableOpacity>
-              <Text
-                style={[
-                  styles.sellerText,
-                  styles.title
-                ]}
-              >
-                Contact the seller through this media : 
-              </Text>
-              {
-                contact.phoneNumber !== '' ?
-                  <MediaComponent
-                    imageSource={require('@/assets/images/whatsapp.png')}
-                    value={contact.phoneNumber}
-                    onPress={() => onClickMedia('whatsapp')}
-                  />
-                  :
-                  null
-              }
-              {
-                contact.instagram !== '' ?
-                  <MediaComponent
-                    imageSource={require('@/assets/images/instagram.png')}
-                    value={contact.instagram}
-                    onPress={() => onClickMedia('instagram')}
-                  />
-                  :
-                  null
-              }
-              {
-                contact.facebook !== '' ?
-                  <MediaComponent
-                    imageSource={require('@/assets/images/facebook.png')}
-                    value={contact.facebook}
-                    onPress={() => onClickMedia('facebook')}
-                  />
-                  :
-                  null
-              }
-            </View>
+      {isBuy ? (
+        <View style={styles.modalContainer}>
+          <View style={styles.box}>
+            <TouchableOpacity onPress={() => setIsBuy(false)} style={styles.cancel}>
+              <AntDesign name="close" size={30} color="black" />
+            </TouchableOpacity>
+            <Text style={[styles.sellerText, styles.title]}>Contact the seller through this media:</Text>
+            {contact.phoneNumber !== '' && (
+              <MediaComponent
+                imageSource={require('@/assets/images/whatsapp.png')}
+                value={contact.phoneNumber}
+                onPress={() => onClickMedia('whatsapp')}
+              />
+            )}
+            {contact.instagram !== '' && (
+              <MediaComponent
+                imageSource={require('@/assets/images/instagram.png')}
+                value={contact.instagram}
+                onPress={() => onClickMedia('instagram')}
+              />
+            )}
+            {contact.facebook !== '' && (
+              <MediaComponent
+                imageSource={require('@/assets/images/facebook.png')}
+                value={contact.facebook}
+                onPress={() => onClickMedia('facebook')}
+              />
+            )}
           </View>
-          :
-          null
-      }
+        </View>
+      ) : null}
     </View>
   );
-}
+};
+
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
